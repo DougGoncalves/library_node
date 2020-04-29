73d782e369505e6012222ff593869db1ae56eaf7
Douglas Gonçalves <douglasgoncalves@MacBook-Air-de-Douglas.local>
2020-04-29 12:05:28 -0300

    FilePond Upload Implementation

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const Book = require('../models/Book');
 
 router.get('/', async (req, res) => {
   let books
   try {
     books = await Book.find().sort({ createdAt: 'desc' }).limit(10).exec();
   } catch {
-    books = []
+    books = [];
   }
   res.render('index', { books: books });
 })

535d66ba5618f55578e2181f816bb001357fc34e
Douglas Gonçalves <douglasgoncalves@MacBook-Air-de-Douglas.local>
2020-05-01 09:47:20 -0300

    CRUD for Books

--- routes/authors.js
+++ routes/authors.js
@@ -41,6 +41,7 @@ router.post('/', async (req, res) => {
   }
 });
 
+//Show Author
 router.get('/:id', async (req, res) => {
   try {
     const author = await Author.findById(req.params.id);
@@ -54,6 +55,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+//Edit Author
 router.get('/:id/edit', async (req, res) => {
   try{
     const author = await Author.findById(req.params.id);
@@ -64,6 +66,7 @@ router.get('/:id/edit', async (req, res) => {
   
 });
 
+//Update Author
 router.put('/:id', async (req, res) => {
   let author
   try {
@@ -83,6 +86,7 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+//Delete Author
 router.delete('/:id', async (req, res) => {
   let author
   try {
